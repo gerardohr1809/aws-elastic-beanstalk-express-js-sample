e7fa57f05832c506eb46580d334f3443b6b40210
gerardohr1809 <94191467+gerardohr1809@users.noreply.github.com>
2021-11-15 22:27:54 -0600

    change from web

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola mundo 2!'));
+app.get('/', (req, res) => res.send('Hola mundo 4!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
